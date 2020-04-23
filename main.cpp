5787e55edc8802a3eab6e03cfc4f8fbc1e8ddc2b
VoidVirtual <58012554+VoidVirtual@users.noreply.github.com>
2020-04-23 13:35:21 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include "Matrix.h"
+#include "ExtendedMatrix.h"
+#include "Vector.h"
+#include <ctime>
+int main(int argc, char** argv)
+{
+    srand(time(0));
+    Matrix A("Matrix.txt");
+    Matrix B = {
+                    {3,0,0},
+                    {0,3,0},
+                    {0,0,3}
+               };
+    ExtendedMatrix C(3,RANDOM|SYMMETRIC);
+    std::cout<<A*B<<"\n";
+    std::cout<<C<<"\n";
+}
